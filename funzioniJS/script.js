379f78dfb44dba392a54bf40611dab320ef0cb86
Aleh6969 <ale.ara2005@gmail.com>
2024-04-18 19:16:40 +0200

    hopefully good changes

--- funzioniJS/script.js
+++ funzioniJS/script.js
@@ -1,21 +1,33 @@
 specialChars =/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/;
-import { 
+/*import { 
     isValid, 
     isExpirationDateValid, 
     isSecurityCodeValid, 
     getCreditCardNameByNumber 
-} from 'creditcard.js';
+} from 'creditcard.js';*/
 function emailValidation(input){
+    console.log("EmailValidation")
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (input.value.match(validRegex)) {
-        document.form1.text1.focus();
-        return true;
-    } else {
-        document.form1.text1.focus();
-        return false;
+    if(input.value!=""){
+        if (input.value.match(validRegex)) {
+            console.log("Email valida")
+            return true;
+        } else {
+            console.log("Email non valida")
+            alert("Inserire una mail valida")
+            return false;
+        }
     }
 }
-function CCValidate(CCNum, ExpDate, CVC, HolderName){
+function CambiaTrasparenza(){
+    let a = document.getElementById("divForm");
+    if(a.style.visibility != "visible"){
+        a.style.visibility = "visible";
+    }else{
+        a.style.visibility = "hidden";
+    }
+}
+/*function CCValidate(CCNum, ExpDate, CVC, HolderName){
     isValid('4916108926268679'); // returns true
     isExpirationDateValid('02', '2027'); // returns true
     isSecurityCodeValid('4556603578296676', '250'); // returns true
@@ -24,3 +36,4 @@ function CCValidate(CCNum, ExpDate, CVC, HolderName){
         alert("inserire un nome valido");
     }
 }
+*/
