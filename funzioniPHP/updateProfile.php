1e19a22cce581bd847e2a56280fefcc8ea09e934
Aleh6969 <ale.ara2005@gmail.com>
2024-05-02 09:53:45 +0200

    tentativo 2

--- funzioniPHP/updateProfile.php
+++ funzioniPHP/updateProfile.php
@@ -13,38 +13,38 @@ $username = $_POST['username'] ?? '';
 $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
 function updateUsername($pdo, $userid, $username) {
-    $sqlUsername = "UPDATE Cliente SET UserName = ':username' WHERE CODCLN = :userid";
-    if(!empty(trim($username))) {
+    $sqlUsername = "UPDATE Cliente SET UserName = '$username' WHERE CODCLN = $userid";
+    //if(!empty(trim($username))) {
         $stmt1 = $pdo->prepare($sqlUsername);
-        $stmt1->bindParam(':username', $username);
-        $stmt1->bindParam(':userid', $userid);
+        //$stmt1->bindParam(':username', $username);
+        //$stmt1->bindParam(':userid', $userid);
         if(!$stmt1->execute()) {
             throw new Exception('Error updating username');
         }
     }
-}
+//}
 function updatePassword($pdo, $userid, $password) {
-    $sqlPassword = "UPDATE Cliente SET UserPassword = ':password' WHERE CODCLN = :userid";
-    if(!empty(trim($password))) {
+    $sqlPassword = "UPDATE Cliente SET UserPassword = '$password' WHERE CODCLN = $userid";
+    //if(!empty(trim($password))) {
         $stmt2 = $pdo->prepare($sqlPassword);
-        $stmt2->bindParam(':password', $password);
-        $stmt2->bindParam(':userid', $userid);
+        //$stmt2->bindParam(':password', $password);
+        //$stmt2->bindParam(':userid', $userid);
         if(!$stmt2->execute()) {
             throw new Exception('Error updating password');
         }
     }
-}
+//}
 function updateEmail($pdo, $userid, $email) {
-    $sqlEmail = "UPDATE Cliente SET UserEmail = ':email' WHERE CODCLN = :userid";
-    if(!empty(trim($email))) {
+    $sqlEmail = "UPDATE Cliente SET UserEmail = '$email' WHERE CODCLN = $userid";
+    //if(!empty(trim($email))) {
         $stmt3 = $pdo->prepare($sqlEmail);
-        $stmt3->bindParam(':email', $email);
-        $stmt3->bindParam(':userid', $userid);
+        //$stmt3->bindParam(':email', $email);
+        //$stmt3->bindParam(':userid', $userid);
         if(!$stmt3->execute()) {
             throw new Exception('Error updating email');
         }
     }
-}
+//}
 try {
     if(isset($_POST["username"])) {
         updateUsername($pdo, $userid, $username);
